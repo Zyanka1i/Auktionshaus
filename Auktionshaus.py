5af4de4738e391feaaf79d82be74500ade8ce468
anonDonau <49351948+anonDonau@users.noreply.github.com>
2019-08-11 23:18:11 +0200

    Add files via upload
    it should have an option to add items with a ranging price with auctions and also deleting them files automatically when the auction ran out and the item is sold

---
+++ Auktionshaus.py
@@ -0,0 +1,13 @@
+#Kassensystem für Versteigerungen alter Artefakte des /
+# 2. Weltkrieges. 
+
+print("Willkommen im Auktionshaus!")
+
+#Liste der Artefakte
+Artefakt1= ["SS Dolch", 1939, 80]
+Artefakt2= ["Reichsflagge", 1933, 70]
+Artefalt3= ["Eisernes Kreuz", 1921, 15]
+ListeArtefakte = [Artefakt1, Artefakt2, Artefalt3]
+
+print(ListeArtefakte)
+print(input())
